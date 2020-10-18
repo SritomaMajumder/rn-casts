b9d2d72cc80912d219f65d704cc97f4c603f5905
SritomaMajumder <sritoma.majumder@gmail.com>
2020-10-18 15:12:34 +0200

    components

---
+++ albums/src/components/ClassComponent.js
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class AlbumList extends Component {
+  componentDidMount() {
+    console.log('componentDidMount in AlbumList');
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!</Text>
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
---
+++ albums/src/components/FunctionalComponent.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const FunctionalComponent = () => {
+    return (
+        <View>
+            <Text>FunctionalComponent !!!</Text>
+        </View>
+    );
+};
+
+export default FunctionalComponent;
