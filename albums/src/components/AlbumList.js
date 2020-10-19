a9759ad6b9143b1b6fae05c9ad79d97af8975815
SritomaMajumder <sritoma.majumder@gmail.com>
2020-10-19 19:02:29 +0200

    final

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,19 +1,30 @@
 /* eslint-disable prettier/prettier */
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {ScrollView} from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = {albums: []};
+
   componentDidMount() {
-      console.log('componentDidMount in AlbumList');
+    axios
+      .get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then((response) => this.setState({albums: response.data}));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map((album) => (
+      <AlbumDetail key={album.title} album={album}/>
+    ));
   }
 
   render() {
-    return (
-      <View>
-        <Text>Album List !!!</Text>
-      </View>
-    );
+    console.log(this.state);
+
+    return <ScrollView>{this.renderAlbums()}</ScrollView>;
   }
 }
+
 
 export default AlbumList;
